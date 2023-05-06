7990a99d3af93d16b112894205ca29e29a9d0cea
alezamarion <alezamarion@icloud.com>
2023-05-06 17:53:04 -0300

    main ex01

--- Module_00/ex01/main.cpp
+++ Module_00/ex01/main.cpp
@@ -6,7 +6,42 @@
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/06 16:03:52 by azamario          #+#    #+#             */
-/*   Updated: 2023/05/06 16:03:54 by azamario         ###   ########.fr       */
+/*   Updated: 2023/05/06 17:52:48 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PhoneBook.hpp"
+#include <string>
+
+
+bool fieldValidation(std::string input, std::string fieldName);
+
+bool phoneValidation(std::string input, std::string fieldName);
+
+void addContact(PhoneBook &objPhoneBook);
+
+void searchContact(PhoneBook objPhoneBook);
+
+
+int main(void)
+{
+    PhoneBook objPhoneBook;
+    std::string option;
+
+    while (1)
+    {
+        std::cout << "Choose one option: " << std::endl;
+        std::cout << "Option: ADD | SEARCH | EXIT" << std::endl;
+        std::cin >> option;
+
+        if (option == "ADD")
+            addContact(objPhoneBook);
+        else if (option == "SEARCH")
+            searchContact(objPhoneBook);
+        else if (option == "EXIT")
+            break;
+        else
+            std::cout << "Invalid option" << std::endl;
+    }
+    return (0);
+}
