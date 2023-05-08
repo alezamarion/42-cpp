7f0e4daea141adec8dc2956ea4250976cad347c3
alezamarion <alezamarion@icloud.com>
2023-05-08 10:54:49 -0300

    Module02 ex00

---
+++ Module02/ex00/Fixed.hpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/08 10:37:02 by azamario          #+#    #+#             */
+/*   Updated: 2023/05/08 10:54:21 by azamario         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+
+class Fixed
+{
+    public:
+        Fixed(void);
+        Fixed(Fixed const &src);
+        ~Fixed(void);
+        
+        Fixed &operator=(Fixed const &src);
+        
+        int getRawBits(void) const;
+        void setRawBits(int const raw);
+        
+    private:
+        int _rawBits;
+        static int const _fractionalBits;
+};
+
+std::ostream &operator<<(std::ostream &o, Fixed const &i); //não foi implementado?
+
+#endif
