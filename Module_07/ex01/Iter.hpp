8042c739cfa08c6302274502288fd0669a660213
alezamarion <alezamarion@icloud.com>
2023-05-16 18:13:54 -0300

    Module_07 ex00

--- Module_07/ex01/Iter.hpp
+++ Module_07/ex01/Iter.hpp
@@ -1,40 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*   Iter.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/16 17:05:00 by azamario          #+#    #+#             */
-/*   Updated: 2023/05/16 18:12:46 by azamario         ###   ########.fr       */
+/*   Created: 2023/05/16 18:08:54 by azamario          #+#    #+#             */
+/*   Updated: 2023/05/16 18:08:55 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WHATEVER_CPP
-#define WHATEVER_CPP
-
-#include <iostream>
-
-template <typename T>
-void swap(T &a, T &b)
-{
-    T temp;
-    
-    temp = a;
-    a = b;
-    b = temp;
-}
-
-template <typename T>
-const T& min(const T &a, const T &b)
-{
-    return (a < b) ? a : b;
-}
-
-template <typename T>
-const T& max(const T &a, const T &b)
-{
-    return (a > b) ? a : b;
-}
-
-#endif
