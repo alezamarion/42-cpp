faeaced17698eff833f71020bf4aeafdd6f012f1
alezamarion <alezamarion@icloud.com>
2023-05-17 16:49:02 -0300

    Module_07 ex02

--- Module_07/ex02/main.cpp
+++ Module_07/ex02/main.cpp
@@ -6,13 +6,73 @@
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 20:55:03 by azamario          #+#    #+#             */
-/*   Updated: 2023/05/17 11:49:48 by azamario         ###   ########.fr       */
+/*   Updated: 2023/05/17 16:47:11 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Array.hpp"
 
+int main(void)
+{
+    Array<char> a(10);
+    Array<int> b(10);
+    Array<float> c(10);
+    Array<float> x(c);
+    Array<std::string> d(5);
+    Array<int> e(0);
+
+    std::cout << "*** CHAR ***\n";
+    for (size_t i = 0; i < 10; i++)
+        a[i] = static_cast<char>(i + 65);
+    
+    std::cout << a << "\n";
+
+    std::cout << "*** INT ***\n";
+    for (size_t i = 0; i < 10; i++)
+        b[i] = static_cast<int>(i + 1);
+
+    std::cout << b << "\n";
+    
+    std::cout << "*** FLOAT ***\n";    
+    for (size_t i = 0; i < 10; i++)
+        x[i] = static_cast<float>(1.1f + static_cast<float>(i));
+
+     std::cout << x << "\n";
+
+     std::cout << "*** STRING ***\n";
+
+    for (size_t i = 0; i < 5; i++)
+        d[i] = std::string("str") + static_cast<char> (i + '0' + 1);
+    
+    std::cout << d;
+
+    std::cout << "\n";
+
+    std::cout << "*** Out of Range index ***\n";
+    try
+    {
+        e[100];
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what();
+    }
+    try
+    {
+        e[-1];
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    return (0);
+}
+
+
+
+
+/*
 int main(void)
 {
     Array<int> a(10);
@@ -78,7 +138,7 @@ int main(void)
 
     return (0);
 }
-
+*/
 /*
 
 #define MAX_VAL 750
