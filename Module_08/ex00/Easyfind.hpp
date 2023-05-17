5630ee8a9713ce7cabe4021f6c894853545a7c83
alezamarion <alezamarion@icloud.com>
2023-05-17 20:57:07 -0300

    Module_08 ex00

--- Module_08/ex00/Easyfind.hpp
+++ Module_08/ex00/Easyfind.hpp
@@ -6,7 +6,33 @@
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/17 11:59:05 by azamario          #+#    #+#             */
-/*   Updated: 2023/05/17 11:59:06 by azamario         ###   ########.fr       */
+/*   Updated: 2023/05/17 20:56:17 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+
+#include <algorithm>
+#include <deque>
+#include <exception>
+#include <list>
+#include <string>
+#include <vector>
+#include <iostream>
+
+
+template <typename T>
+typename T::iterator easyfind(T &container, int value)
+{
+    typename T::iterator iter = std::find(container.begin(), container.end(), value);
+    
+    if(iter == container.end())
+    {
+        throw std::runtime_error(std::string("Value not found\n"));
+    }
+    return (iter);
+}
+
+#endif
