6bf181a9673e3e7d7916842eb7ac3ac0924d29e4
alezamarion <alezamarion@icloud.com>
2023-05-22 10:56:41 -0300

    Module 00 ex00

--- Module_00/ex00/megaphone.sh
+++ Module_00/ex00/megaphone.sh
@@ -19,3 +19,24 @@ $EXE
 echo "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
 
 make fclean &>/dev/null
+
+
+echo
+: <<'END'
+
+The first line is called a "shebang" and specifies the interpreter to be used for executing the script. 
+In this case, it is set to /bin/bash, indicating that the script should be interpreted by the Bash shell.
+
+EXE="./megaphone" -> This line sets the value of the variable EXE to the string "./megaphone". 
+It likely represents the path or name of an executable file that will be invoked later.
+
+It prints "Test 1:" to the console. Then it executes the command stored in the variable EXE 
+with the argument "shhhhh... I think the students are asleep...". 
+The output is then printed using echo. The expected output is "SHHHHH... I THINK THE STUDENTS ARE ASLEEP...".
+
+The make fclean command, likely to clean up any generated files or artifacts created during the build process. 
+The &>/dev/null portion redirects both standard output and standard error to /dev/null, 
+discarding any output that might be generated.
+
+END
+echo
