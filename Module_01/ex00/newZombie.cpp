cd9c9ba7bdf481941e85c66939d30d33969c7235
alezamarion <alezamarion@icloud.com>
2023-05-22 22:21:34 -0300

    that files were necessary :D

--- Module_01/ex00/newZombie.cpp
+++ Module_01/ex00/newZombie.cpp
@@ -1,28 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/07 15:54:26 by azamario          #+#    #+#             */
-/*   Updated: 2023/05/22 22:19:15 by azamario         ###   ########.fr       */
+/*   Created: 2023/05/22 22:18:44 by azamario          #+#    #+#             */
+/*   Updated: 2023/05/22 22:18:59 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie(std::string name)
+Zombie* newZombie(std::string name)
 {
-   this->_name = name;
-}
-
-Zombie::~Zombie(void)
-{
-    std::cout << _name << " Arghhhh \033[0;31mdead :(\033[0m\n" << std::endl;
-}
-
-void Zombie::announce(void)
-{
-    std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+    return new Zombie(name);
 }
--- Module_01/ex00/randomChump.cpp
+++ Module_01/ex00/randomChump.cpp
@@ -1,28 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*   randomChump.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/07 15:54:26 by azamario          #+#    #+#             */
-/*   Updated: 2023/05/22 22:19:15 by azamario         ###   ########.fr       */
+/*   Created: 2023/05/22 22:18:01 by azamario          #+#    #+#             */
+/*   Updated: 2023/05/22 22:19:36 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie::Zombie(std::string name)
+void randomChump(std::string _name)
 {
-   this->_name = name;
-}
-
-Zombie::~Zombie(void)
-{
-    std::cout << _name << " Arghhhh \033[0;31mdead :(\033[0m\n" << std::endl;
-}
-
-void Zombie::announce(void)
-{
-    std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+    Zombie z(_name);
+    z.announce();
 }
