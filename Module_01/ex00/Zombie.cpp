b21d4d8a7534ae44fe40e0f6a087267e030fc88f
alezamarion <alezamarion@icloud.com>
2023-05-22 22:11:27 -0300

    Module_01 ex00: less files

--- Module_01/ex00/Zombie.cpp
+++ Module_01/ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 15:54:26 by azamario          #+#    #+#             */
-/*   Updated: 2023/05/07 16:34:35 by azamario         ###   ########.fr       */
+/*   Updated: 2023/05/22 22:09:28 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,21 @@ Zombie::Zombie(std::string name)
 
 Zombie::~Zombie(void)
 {
-    std::cout << _name << " Arghhhh (\033[0;31mdead :(\033[0m)" << std::endl;
+    std::cout << _name << " Arghhhh \033[0;31mdead :(\033[0m\n" << std::endl;
 }
 
 void Zombie::announce(void)
 {
     std::cout << _name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie* newZombie(std::string name)
+{
+    return new Zombie(name);
+}
+
+void randomChump(std::string _name)
+{
+    Zombie z(_name);
+    z.announce();
 }
