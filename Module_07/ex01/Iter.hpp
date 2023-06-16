29e238766fdbca571015ef33fd57a6a89df3c245
alezamarion <alezamarion@icloud.com>
2023-06-16 08:59:01 -0300

    updating main

--- Module_07/ex01/Iter.hpp
+++ Module_07/ex01/Iter.hpp
@@ -6,7 +6,7 @@
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 18:08:54 by azamario          #+#    #+#             */
-/*   Updated: 2023/05/17 10:48:09 by azamario         ###   ########.fr       */
+/*   Updated: 2023/06/15 22:03:46 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,25 @@
 
 #include <iostream>
 
+/*
+    This template function iter allows you to iterate over an array of any type and apply a given function to each element.
+    The function pointer f provides flexibility to pass any function that matches the required signature:
+    (void (*)(const T&)). This function can be useful when you want to perform some operation on each element of an array 
+    without having to write a separate loop each time.
+*/
+
 template <typename T>
 void iter(T* array, size_t lenght, void (*f)(const T&))
 {
     for(size_t i = 0; i < lenght; i++)
         f(array[i]);
 }
+
+/*
+    The printArray function is designed to be used with different types of variables. 
+    It relies on the fact that the << operator is overloaded for the appropriate types to enable printing to the standard 
+    output stream. 
+*/
 
 template <typename T>
 void printArray(const T& n)
--- Module_07/ex01/main.cpp
+++ Module_07/ex01/main.cpp
@@ -6,51 +6,22 @@
 /*   By: azamario <azamario@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 17:49:26 by azamario          #+#    #+#             */
-/*   Updated: 2023/05/17 10:54:32 by azamario         ###   ########.fr       */
+/*   Updated: 2023/06/16 08:56:39 by azamario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Iter.hpp"
 
-/*
-
-int main(void)
-{
-    int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    char arr2[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
-    float arr3[10] = {0.0f, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f};
-
-    std::cout << "[ INT ARRAY ]\n";
-    iter(arr, 10, printArray);
-    std::cout << "\n";
-
-    std::cout << "\n";
-
-    std::cout << "[ CHAR ARRAY ]\n";
-    iter(arr2, 10, printArray);
-    std::cout << "\n";
-
-    std::cout << "\n";
-
-    std::cout << "[ FLOAT ARRAY ]\n";
-    iter(arr3, 10, printArray);
-    std::cout << "\n";
-
-    return (0);
-}
-
-*/
-
 int main( void )
 {
-
 	{
 		int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		int length = sizeof(array) / sizeof(array[0]);
 
 		std::cout << std::endl << "Int   array: ";
 
-		iter(array, length, printArray<int>); //chamou explicitamente
+    	// explicit instantiation -> preferred
+		iter(array, length, printArray<int>); 
 
 		std::cout << std::endl;
 	}
@@ -61,7 +32,8 @@ int main( void )
 
 		std::cout << std::endl << "Float array: ";
 
-		iter(array, length, printArray); //chamou implicitamente
+    	// implicit instantiation -> it might not work for complex classes
+		iter(array, length, printArray); 
 
   		std::cout << std::endl;
 	}
@@ -78,5 +50,4 @@ int main( void )
 	}
 
     return (0);
-
 }
